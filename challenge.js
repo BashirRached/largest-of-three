6313cebead52b9948ca3286bb49a096174a4398e
Bashir Rashid <karche475b@gmail.com>
2023-08-02 09:20:43 +0300

    Solution of the LARGEST-OFTHREE activity

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let a = 200
+let b = 300
+let c = 400
+
+if(a > b && a > c)
+{
+     console.log(`The greatest number of the three is ${a}`)
+}
+else if(b > a && b >c )
+{
+     console.log(`The greatest number of the three is ${b}`)
+}
+else if(c > a && c > b)
+{
+     console.log(`The greatest number of the three is ${c}`)
+}
